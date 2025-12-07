5724d4d00785bfceec24d019d61d1bccf7a47a1a
Mei <lovec8c81@gmail.com>
2025-12-07 15:29:53 +0800

    建制專案

---
+++ FallDetect/FallDetect/FallDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FallDetectApp.swift
+//  FallDetect
+//
+//  Created by 張郁眉 on 2025/12/7.
+//
+
+import SwiftUI
+
+@main
+struct FallDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
